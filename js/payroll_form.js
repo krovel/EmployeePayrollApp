7a424eb8b1e749e1752c3b77bc498ccf3a10df30
krove <krove@DESKTOP-Q5SED1R>
2020-11-23 00:57:29 +0530

    [Kashif]Added:Employee Payroll App Homepage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -23,16 +23,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
     
     const startDate = document.querySelector("#startDate");
-    const day = document.getElementById("day").value;
-    const month = document.getElementById("month").value;
-    const year = document.getElementById("year").value;
+    const day = document.querySelector("#day");
+    const month = document.querySelector("#month");
+    const year = document.querySelector("#year");
     const dateError = document.querySelector(".date-error");
     startDate.addEventListener("input", async function(){
        try{
-         (new EmployeePayrollData()).startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+       new EmployeePayrollData().startDate = new Date( Date.UTC(year.value, month.value - 1, day.value));
         dateError.textContent = "";
       }catch(e){
-        dateError.textContent = "Invalid Date !";
+        dateError.textContent = e;
       }
     });
     });
@@ -54,13 +54,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
       alert(employeeList.toString());
       localStorage.setItem("EmployeePayrollList", JSON.stringify(employeeList));
     }
+    const getSelectedValues = (property) => {
+      let allItems = document.querySelectorAll(property);
+      let setItems = [];
+      allItems.forEach(item => {
+        if(item.checked) setItems.push(item.value);
+      });
+      return setItems;
+    };
     
     function saveData(){
       let employee = new EmployeePayrollData();
       employee.name= document.getElementById("name").value;
       employee.picture = document.querySelector('input[name = profile]:checked').value;
       employee.gender = document.querySelector('input[name = gender]:checked').value;
-      employee.department =document.querySelector('input[name = department]:checked').value;
+      employee.department =getSelectedValues('[name=department]');
       employee.salary = document.getElementById("salary").value;
      var day = document.getElementById("day").value;
      var month = document.getElementById("month").value;
